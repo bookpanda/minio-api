eabe02a37643d2b7d453241543add402729961f4
Idhibhat Pankam <marginpankam@gmail.com>
2024-06-09 23:06:41 +0700

    fix: gf 3 secs

--- cmd/main.go
+++ cmd/main.go
@@ -73,14 +73,14 @@ func main() {
 	<-quit
 	log.Println("Shutdown Server ...")
 
-	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	ctx, cancel := context.WithTimeout(context.Background(), 3*time.Second)
 	defer cancel()
 	if err := srv.Shutdown(ctx); err != nil {
 		log.Fatal("Server Shutdown:", err)
 	}
 	select {
 	case <-ctx.Done():
-		log.Println("timeout of 5 seconds.")
+		log.Println("timeout of 3 seconds.")
 	}
 	log.Println("Server exiting")
 }
