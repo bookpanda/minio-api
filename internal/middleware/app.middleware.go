f7ee8512610d11f0d5d528b018a9db8cc52e0668
Idhibhat Pankam <marginpankam@gmail.com>
2024-06-09 22:15:51 +0700

    feat; middleware

--- internal/middleware/app.middleware.go
+++ internal/middleware/app.middleware.go
@@ -1,1 +1,37 @@
 package middleware
+
+import (
+	"strings"
+
+	"github.com/bookpanda/minio-api/config"
+	"github.com/bookpanda/minio-api/errors"
+	"github.com/gin-gonic/gin"
+)
+
+type AppMidddleware gin.HandlerFunc
+
+func NewAppMiddleware(conf *config.Config) AppMidddleware {
+	return func(c *gin.Context) {
+		authHeader := c.GetHeader("Authorization")
+		if authHeader == "" {
+			errors.ResponseError(c, errors.Unauthorized)
+			c.Abort()
+			return
+		}
+
+		if !strings.HasPrefix(authHeader, "Bearer ") {
+			errors.ResponseError(c, errors.InvalidToken)
+			c.Abort()
+			return
+		}
+
+		token := strings.TrimPrefix(authHeader, "Bearer ")
+		if token != conf.App.ApiKey {
+			errors.ResponseError(c, errors.InvalidToken)
+			c.Abort()
+			return
+		}
+
+		c.Next()
+	}
+}
