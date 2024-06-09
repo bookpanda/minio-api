5aaa6c8938bd8d5835a1d4cab300170570b7db99
Idhibhat Pankam <marginpankam@gmail.com>
2024-06-09 22:16:06 +0700

    feat: router

--- internal/router/router.go
+++ internal/router/router.go
@@ -1,1 +1,23 @@
 package router
+
+import (
+	"github.com/bookpanda/minio-api/config"
+	"github.com/bookpanda/minio-api/internal/middleware"
+	"github.com/gin-gonic/gin"
+)
+
+type Router struct {
+	*gin.Engine
+}
+
+func New(conf *config.Config, corsHandler config.CorsHandler, appMiddleware middleware.AppMidddleware) *Router {
+	if !conf.App.IsDevelopment() {
+		gin.SetMode(gin.ReleaseMode)
+	}
+
+	r := gin.Default()
+	r.Use(gin.HandlerFunc(corsHandler))
+	r.Use(gin.HandlerFunc(appMiddleware))
+
+	return &Router{r}
+}
