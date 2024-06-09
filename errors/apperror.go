2b144174ca6e1c6261b5967b2c1e6f25e1b6ab36
Idhibhat Pankam <marginpankam@gmail.com>
2024-06-09 22:15:24 +0700

    feat: errors

--- errors/apperror.go
+++ errors/apperror.go
@@ -1,1 +1,19 @@
 package errors
+
+import "net/http"
+
+type AppError struct {
+	Id       string
+	HttpCode int
+}
+
+func (e *AppError) Error() string {
+	return e.Id
+}
+
+var (
+	InternalError = &AppError{"Internal error", http.StatusInternalServerError}
+	Unauthorized  = &AppError{"Unauthorized", http.StatusUnauthorized}
+	BadRequest    = &AppError{"Bad request", http.StatusBadRequest}
+	InvalidToken  = &AppError{"Invalid token", http.StatusUnauthorized}
+)
--- errors/error_response.go
+++ errors/error_response.go
@@ -1,1 +1,7 @@
 package errors
+
+import "github.com/gin-gonic/gin"
+
+func ResponseError(c *gin.Context, err *AppError) {
+	c.JSON(err.HttpCode, gin.H{"error": err.Error()})
+}
