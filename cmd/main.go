1dabc511324452e87547a52506e78765c4e0c8b9
Idhibhat Pankam <marginpankam@gmail.com>
2024-06-09 21:11:09 +0700

    feat: config

--- cmd/main.go
+++ cmd/main.go
@@ -1,3 +1,14 @@
 package cmd
 
-func main() {}
+import (
+	"fmt"
+
+	"github.com/bookpanda/minio-api/config"
+)
+
+func main() {
+	_, err := config.LoadConfig()
+	if err != nil {
+		panic(fmt.Sprintf("Failed to load config: %v", err))
+	}
+}
--- config/config.go
+++ config/config.go
@@ -1,1 +1,53 @@
 package config
+
+import (
+	"os"
+
+	"github.com/joho/godotenv"
+)
+
+type AppConfig struct {
+	Port   string
+	Env    string
+	ApiKey string
+}
+
+type StoreConfig struct {
+	Endpoint  string
+	AccessKey string
+	SecretKey string
+	UseSSL    bool
+}
+
+type Config struct {
+	AppConfig   AppConfig
+	StoreConfig StoreConfig
+}
+
+func LoadConfig() (*Config, error) {
+	err := godotenv.Load(".env")
+	if err != nil {
+		return nil, err
+	}
+
+	appConfig := AppConfig{
+		Port:   os.Getenv("APP_PORT"),
+		Env:    os.Getenv("APP_ENV"),
+		ApiKey: os.Getenv("APP_API_KEY"),
+	}
+	storeConfig := StoreConfig{
+		Endpoint:  os.Getenv("STORE_ENDPOINT"),
+		AccessKey: os.Getenv("STORE_ACCESS_KEY"),
+		SecretKey: os.Getenv("STORE_SECRET_KEY"),
+		UseSSL:    os.Getenv("STORE_USE_SSL") == "true",
+	}
+
+	return &Config{
+		AppConfig:   appConfig,
+		StoreConfig: storeConfig,
+	}, nil
+}
+
+func (ac *AppConfig) IsDevelopment() bool {
+	return ac.Env == "development"
+}
