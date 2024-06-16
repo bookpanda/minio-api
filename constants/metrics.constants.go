31527e6b2d59c5566f66ead0e564782edf7ef839
Idhibhat Pankam <marginpankam@gmail.com>
2024-06-16 17:37:00 +0700

    fix: remove comments

--- constants/metrics.constants.go
+++ constants/metrics.constants.go
@@ -23,17 +23,3 @@ const (
 func (m Method) String() string {
 	return string(m)
 }
-
-// type StatusCode uint16
-
-// const (
-// 	OK   StatusCode = 200
-// 	_400 StatusCode = 400
-// 	_401 StatusCode = 401
-// 	_404 StatusCode = 404
-// 	_500 StatusCode = 500
-// )
-
-// func (s StatusCode) String() string {
-// 	return s.String()
-// }
